3edfce89e955411a471cbc76087f01070494f099
Ahmad61-6 <ahmadmizan210@gmail.com>
2026-01-12 03:42:43 +0600

    started the project

---
+++ lib/app/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF1F41BB);
+  static const Color filledBackgroundColor = Color(0xFFF1F4FF);
+  static const Color catBackgroundColor = Color(0xFFF2F2F2);
+}
